dbc4f18a004ce3fd82fcb025d56f009318f12083
alazif123 <alazif@yeah.net>
2019-04-11 04:41:43 +0000

    关注和粉丝列表页面

--- resources/views/shared/_stats.blade.php
+++ resources/views/shared/_stats.blade.php
@@ -1,17 +1,17 @@
-<a href="#">
+<a href="{{ route('users.followings', $user->id) }}">
     <strong id="following" class="stat">
         {{ count($user->followings) }}
     </strong>
     关注
 </a>
-<a href="#">
+<a href="{{ route('users.followers', $user->id) }}">
     <strong id="followers" class="stat">
         {{ count($user->followers) }}
     </strong>
     粉丝
 </a>
-<a href="#">
-    <strong id="statuses" class="stat">
+<a href="{{ route('users.show', $user->id) }}">
+    <strong id="statuses" c lass="stat">
         {{ $user->statuses()->count() }}
     </strong>
     微博
